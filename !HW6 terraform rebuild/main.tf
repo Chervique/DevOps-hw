6a6374e990897b3155e38bf1785f24be1d2407bb
Andriy <andriy.tymoshenko@dev.pro>
2021-11-16 07:15:25 +0200

    dynamic inventory

--- !HW6 terraform rebuild/main.tf
+++ !HW6 terraform rebuild/main.tf
@@ -42,6 +42,7 @@ resource "aws_key_pair" "atym" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
   
   tags = {
     Name = "NEW"
--- !HW7 ansible/start.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-ssh-agent bash
-cp 'AWS atym.pem' ~/.ssh/'AWS atym.pem'
-chmod 400 'AWS atym.pem'
-ssh-add ~/.ssh/'AWS atym.pem'
