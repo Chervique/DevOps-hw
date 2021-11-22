ffe911cfd28a57e998da873cc812fcbd66557564
Andriy <andriy.tymoshenko@dev.pro>
2021-11-22 20:35:04 +0200

    docker-2

--- !HW8 docker/php/test.php
+++ !HW8 docker/php/test.php
@@ -3,10 +3,10 @@
 <h2>LEMP Stack Test</h2> 
 </head> 
 <body> 
-<?php echo '<p>Hello,</p>'; // Define PHP variables for the MySQL connection.  
-$servername = "192.168.0.13"; $username = "root"; $password = "123"; // Create a MySQL connection.  
-$conn = mysqli_connect($servername, $username, $password); // Report if the connection fails or is successful.  
+<?php echo '<p>Hello,</p>';   
+$servername = "192.168.0.13"; $username = "root"; $password = "123"; 
 
+$conn = mysqli_connect($servername, $username, $password);  
 if (!$conn) { 
 exit('<p>Your connection has failed.<p>' . mysqli_connect_error()); 
 } 
